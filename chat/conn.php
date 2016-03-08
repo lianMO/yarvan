9cb27b07090080e20b49034c3ed6336f6d46f8de
mozijie <997627480@qq.com>
2016-03-08 22:57:49 +0800

    delete mysql and chat

--- chat/conn.php
+++
@@ -1,6 +0,0 @@
-<?php 
-@header('content-type:text/html;charset=urf-8');
-mysql_connect('localhost','root','76a4aa3224') or die('数据库连接失败：'.mysql_error());
-mysql_select_db('yarvan') or die(mysql_error());
-mysql_query('set names utf8');
-?>
--- chat/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'chat.class.php';
-	$chat = new Chat();
-	if($id=$_POST['messageId']){
-		$chat->updateStatus($id);
-	}
-	
-	
-?>
